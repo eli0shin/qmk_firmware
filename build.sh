fb8e977d9e714a90f1f2780c39e9c85de7eaa54a
Eli Oshinsky <elimelech.oshinsky@gmail.com>
2024-05-12 18:13:12 -0400

    add macro mapper

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! bin/bash
-
-qmk compile ./dactyl_layout.json
+./qmk_macro_mapper/macro_mapper ./bare_layout.json ./macros.json ./dactyl_built_layout.json
+qmk compile ./dactyl_build_layout.json
 qmk flash .build/handwired_dactyl_manuform_4x5_handwired_dactyl_manuform_4x5_layout_mine.hex
