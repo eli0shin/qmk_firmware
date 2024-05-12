48ed56313eb045d207b9f962e136acad9616f4ac
Eli Oshinsky <elimelech.oshinsky@gmail.com>
2024-05-12 18:19:58 -0400

    remove debug messages, fix build target file

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! bin/bash
 ./qmk_macro_mapper/macro_mapper ./bare_layout.json ./macros.json ./dactyl_built_layout.json
-qmk compile ./dactyl_build_layout.json
+qmk compile ./dactyl_built_layout.json
 qmk flash .build/handwired_dactyl_manuform_4x5_handwired_dactyl_manuform_4x5_layout_mine.hex
